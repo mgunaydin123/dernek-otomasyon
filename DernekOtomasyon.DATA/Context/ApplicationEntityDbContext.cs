166c4e5c4c9740567b164f711caec5234074482b
mgunaydin123 <113054576+mgunaydin123@users.noreply.github.com>
2022-11-01 22:04:25 +0300

    dernek-otomasyon

---
+++ DernekOtomasyon.DATA/Context/ApplicationEntityDbContext.cs
@@ -0,0 +1,45 @@
+﻿using DernekOtomasyon.DATA.Entity;
+using DernekOtomasyon.DATA.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DernekOtomasyon.DATA.Context
+{
+    public class ApplicationEntityDbContext : DbContext
+    {
+        public ApplicationEntityDbContext()
+            : base("DbConnection")
+        {
+
+            //Database.SetInitializer<ApplicationEntityDbContext>(new DropCreateDatabaseIfModelChanges<ApplicationEntityDbContext>());
+
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationEntityDbContext, Configuration>("DbConnection"));
+
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            
+            // or whatever your schema specifies
+
+            //    modelBuilder.Entity<Member>()
+            //.HasRequired<Member>(x => x.)
+            //.WithMany(x => x.HobyID)
+            //.HasForeignKey(x => x.CategoryId);
+
+            //  modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            //   modelBuilder.Entity<MemberFavoriteAdvert>().HasMany(i => i.AdvertID && i.MemberID).WithRequired().WillCascadeOnDelete(false);
+            
+        }
+        public DbSet<Member> Member { get; set; }
+    }
+}
+
---
+++ DernekOtomasyon.MVC/Controllers/Base/BaseController.cs
@@ -0,0 +1,35 @@
+﻿using DernekOtomasyon.Business.DataServices;
+using DernekOtomasyon.Business.UnitOfWork;
+using DernekOtomasyon.DATA.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DernekOtomasyon.MVC.Controllers.Base
+{
+    public class BaseController : Controller
+    {
+        // GET: Base
+
+
+        private ApplicationEntityDbContext _dbContext;
+
+        public MemberRepository MemberRepo;
+
+
+
+        public IUnitOfWork _uow;
+
+        public BaseController()
+        {
+
+            _dbContext = new ApplicationEntityDbContext();
+            MemberRepo = new MemberRepository(_dbContext);
+
+
+            _uow = new EFUnitOfWork(_dbContext);
+        }
+    }
+}
